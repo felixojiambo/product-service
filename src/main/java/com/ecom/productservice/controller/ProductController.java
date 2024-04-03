dc5a8fe735afe0a3a1307bd18cd95751a2ef9256
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-03 01:21:54 -0700

    Testcontainers library for JUnit tests

--- src/main/java/com/ecom/productservice/controller/ProductController.java
+++ src/main/java/com/ecom/productservice/controller/ProductController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
+    @PostMapping("/product")
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
     }
 
-    @GetMapping
+    @GetMapping("/product")
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
