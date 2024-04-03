164da543502101e8e89c7161733c81759b352784
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-02 23:50:17 -0700

    product setup

---
+++ src/main/java/com/ecom/productservice/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.ecom.productservice.repository;
+import com.ecom.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
